3514c650f2912636f06f233d8f54f22286d2e948
viktorlov <orlovva@ymail.com>
2023-04-28 15:39:42 +0300

    (l) code review

--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -1,11 +1,21 @@
+import pytest
+
+import source.transactions
 from source.transactions import load_transactions_from_json, get_last_transactions
 
 
 def test_load_transactions_from_json():
-    assert load_transactions_from_json() == []
-    ...
+    fixture = '../operations.json'
+    assert type(load_transactions_from_json(fixture)) == list
+    assert type(load_transactions_from_json(fixture)[0]) == source.transactions.Transaction
+    assert len(load_transactions_from_json(fixture)) == 90
+    with pytest.raises(FileNotFoundError):
+        assert load_transactions_from_json('some.json')
+
+
 
 
 def test_get_last_transactions():
-    assert get_last_transactions() == []
+    # assert get_last_transactions() == []
+
     ...
